fb69f105d8c3e9ba68bb0e2abc5c981b52ca7595
ramenu <mansoorar@proton.me>
2024-02-08 19:25:55 -0500

    add gnupg to installation

--- stage2.sh
+++ stage2.sh
@@ -136,7 +136,8 @@ install_package  mesa xorg xfce4 opensnitch \
 				 tmux xdg-user-dirs audit bubblewrap \
 				 adwaita-qt5 xfce4-screensaver xarchiver \
 				 zathura-pdf-poppler noto-fonts noto-fonts-cjk \
-				 noto-fonts-emoji urlwatch gvfs-mtp kernel-modules-hook
+				 noto-fonts-emoji urlwatch gvfs-mtp kernel-modules-hook \
+				 gnupg
 
 notify 'Enabling opensnitchd to run at startup'; systemctl enable opensnitchd.service
 notify 'Enabling NetworkManager to run at startup'; systemctl enable NetworkManager.service
